0007ac1fbd1b8e0f6e35387066eb32e013283115
dylanlott <lott.dylan@gmail.com>
2015-04-10 14:37:10 -0600

    added character count

--- js/animations.js
+++ js/animations.js
@@ -7,16 +7,21 @@ $(document).ready(function(){
 		$("#tweet-controls").show(); 
 	})
 
-	$('.tweet-compose').keyup(function () {
-	  var max = 140;
-	  var len = $(this).val().length;
-	  if (len >= max) {
-	    $('#charNum').text(' you have reached the limit');
-	  }
-	  else {
-	 	 var char = max - len;
-	  	 $('#charNum').text(char + ' characters left');
-  }
-});
+	$('.tweet-compose').keyup(function(){
+	  	var max = 140; // set max char count 
+	  	var len = $(this).val().length;
+	 	if (len >= max) {
+	    	$('#char-count').text(' you have reached the limit'); // displays char count
+	    	$('#tweet-submit').hide(); //hide tweet button when char limit reached
+	  	}
+	 	else {
+	 		var char = max - len;
+	  		$('#char-count').text(char);
+	  		$('#tweet-submit').show();
+  		}
+  		return len; 
+	});
+
+	
 
 })
